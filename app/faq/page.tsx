63c741dc5f4d117bf9dd9c2d3551d74b9b83aaaa
Technophile <dostonnabotov@gmail.com>
2025-11-25 16:48:36 +0500

    feat(page): add FAQ section

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,8 +1,38 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import { FAQ_ITEMS } from "@/lib/constants";
+
 export default function Page() {
   return (
     <section>
-      <div className="wrapper py-8">
-        <h1 className="text-3xl font-bold text-center">Coming Soon...</h1>
+      <div className="wrapper py-8 space-y-6">
+        <h1 className="text-3xl font-bold text-center">FAQ</h1>
+        <Accordion
+          className="max-w-2xl w-full mx-auto space-y-2"
+          collapsible
+          defaultValue="1"
+          type="single"
+        >
+          {FAQ_ITEMS.map((item) => (
+            <AccordionItem
+              className="rounded-md border bg-background px-6 py-2 outline-none last:border-b has-focus-visible:border-ring has-focus-visible:ring-[3px] has-focus-visible:ring-ring/50"
+              key={item.id}
+              value={item.id}
+            >
+              <AccordionTrigger className="py-2 text-lg font-semibold leading-6 hover:no-underline focus-visible:ring-0">
+                {item.title}
+              </AccordionTrigger>
+              <AccordionContent className="pb-2 text-base text-muted-foreground">
+                {item.content}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
       </div>
     </section>
   );
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const GITHUB_URL = "https://github.com/technoph1le/iloveyoutube";
 export const SPONSOR_URL = "https://ko-fi.com/technoph1le";
-export const DISCORD_URL = "https:/discord.gg/Nm5K46yUy5";
+export const DISCORD_URL = "https://discord.gg/Nm5K46yUy5";
 export const YOUTUBE_URL = "https://youtube.com/@technoph1le";
 
 export const THUMBNAIL_PREVIEW_CATEGORIES = {
@@ -195,3 +195,49 @@ export const THUMBNAIL_PREVIEW_CATEGORIES = {
     "xZ4uVx5C5yg",
   ],
 };
+
+export const FAQ_ITEMS = [
+  {
+    id: "1",
+    title: "What is iLoveYouTube?",
+    content:
+      "It’s a collection of simple tools that make YouTube creators’ lives easier — thumbnail previewing, thumbnail downloading, metadata checking, video utilities, and more.",
+  },
+  {
+    id: "2",
+    title: "Is this site affiliated with YouTube?",
+    content: "Nope. This is an independent project made for creators.",
+  },
+  {
+    id: "3",
+    title: "Can I convert videos to MP3 or MP4?",
+    content: "This feature is coming soon.",
+  },
+  {
+    id: "4",
+    title: "Do I need an account to use the tools?",
+    content: "No. Everything works without logging in.",
+  },
+  {
+    id: "5",
+    title: "Do you store my data?",
+    content:
+      "Nope. We don’t store thumbnails, images, URLs, or video data. Your uploads stay in your browser.",
+  },
+  {
+    id: "6",
+    title: "Is the service free to use?",
+    content:
+      "Core tools will stay free forever. However, you can still buy the creator a coffee xD.",
+  },
+  {
+    id: "7",
+    title: "I found a bug. How can I report it?",
+    content: "You can reach out through GitHub issues.",
+  },
+  {
+    id: "8",
+    title: "Can I use this on mobile?",
+    content: "Yes, everything is mobile-friendly.",
+  },
+];
